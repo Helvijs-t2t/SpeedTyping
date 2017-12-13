c4529a0e18dadfe58eeb748fba14620e8de90733
helvijs-t2t <helvijso@toptotoe.com>
2017-12-13 21:15:05 +0000

    Added some more code

---
+++ src/StartGame.java
@@ -0,0 +1,171 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
+
+public class StartGame extends JFrame implements ActionListener, MouseListener, KeyListener{
+JFrame gameFrame = new JFrame();
+
+public final int WIDTH = 1200, HEIGHT = 800;
+
+public Renderer renderer;
+
+public Rectangle car;
+
+public ArrayList<Rectangle> columns;
+
+public int ticks, yMotion, score;
+
+public boolean gameOver, started;
+
+public Random rand;
+	
+	public StartGame() {
+		Timer timer = new Timer(20, this);
+		car = new Rectangle(WIDTH / 2 - 4, HEIGHT / 2 - 4, 20, 20);
+		columns = new ArrayList<Rectangle>();
+		
+		gameFrame.add(renderer);
+		gameFrame.setTitle("Speed Typing Alpha");
+		gameFrame.setSize(WIDTH, HEIGHT);
+		
+		gameFrame.addMouseListener(this);
+		gameFrame.addKeyListener(this);
+		
+		gameFrame.setResizable(false);
+		gameFrame.setVisible(true);
+		gameFrame.setLocationRelativeTo(null);
+		gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+		timer.start();
+		
+		gameFrame.setVisible(true);
+	}
+	public void paintColumn(Graphics g, Rectangle column)
+	{
+		g.setColor(Color.green.darker());
+		g.fillRect(column.x, column.y, column.width, column.height);
+	}
+	
+	public void repaint(Graphics g)
+	{
+		g.setColor(Color.cyan);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+
+		g.setColor(Color.orange);
+		g.fillRect(0, HEIGHT - 120, WIDTH, 120);
+
+		g.setColor(Color.green);
+		g.fillRect(0, HEIGHT - 120, WIDTH, 20);
+
+		g.setColor(Color.red);
+		g.fillRect(car.x, car.y, car.width, car.height);
+
+		for (Rectangle column : columns)
+		{
+			paintColumn(g, column);
+		}
+	}
+	
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent btn) {
+
+		
+	}
+
+
+
+
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
