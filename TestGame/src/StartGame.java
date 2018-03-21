199d45523d6cf0483ba2e53d314de70dcc120af0
helvijs-t2t <helvijso@toptotoe.com>
2018-03-21 20:54:31 +0000

    fixed rendering bug

--- TestGame/src/StartGame.java
+++ TestGame/src/StartGame.java
@@ -20,19 +20,11 @@ public class StartGame extends JFrame implements ActionListener, KeyListener{
 JFrame gameFrame = new JFrame();
 public static StartGame Startgame;
 public final int WIDTH = 1200, HEIGHT = 800;
-public Renderer renderer;
-public Rectangle car;
-public ArrayList<Rectangle> columns;
-public int ticks, yMotion, score;
 public boolean gameOver, started;
-public Random rand;
 
 	public StartGame() {
 		Timer timer = new Timer(20, this);
-		car = new Rectangle(WIDTH / 2 - 4, HEIGHT / 2 - 4, 20, 20);
-		columns = new ArrayList<Rectangle>();
 		
-		//gameFrame.add(renderer);
 		gameFrame.setTitle("Speed Typing Alpha");
 		gameFrame.setSize(WIDTH, HEIGHT);
 		gameFrame.addKeyListener(this);
@@ -45,31 +37,10 @@ public Random rand;
 		
 		gameFrame.setVisible(true);
 	}
-	public void paintColumn(Graphics g, Rectangle column)
-	{
-		g.setColor(Color.green.darker());
-		g.fillRect(column.x, column.y, column.width, column.height);
-	}
 	
-	public void repaint(Graphics g)
-	{
-		g.setColor(Color.cyan);
-		g.fillRect(0, 0, WIDTH, HEIGHT);
-
-		g.setColor(Color.orange);
-		g.fillRect(0, HEIGHT - 120, WIDTH, 120);
-
-		g.setColor(Color.green);
-		g.fillRect(0, HEIGHT - 120, WIDTH, 20);
-
-		g.setColor(Color.red);
-		g.fillRect(car.x, car.y, car.width, car.height);
-
-		for (Rectangle column : columns)
-		{
-			paintColumn(g, column);
-		}
-	}
+	
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent btn) {
 
